0cc9c96d8cdebbda4302d7462f41957b1062dde5
c3m7 <cem@cetinkaya.at>
2025-05-23 15:47:12 +0200

    Test 23.5.25

--- REACT/01/demo/src/App.jsx
+++ REACT/01/demo/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Todo from './components/TodoTest/Todo'
-import TodoList from './components/TodoTest/TodoList'
-import Counter from './components/Test 20.5.25/Counter'
 import Test from './components/Test 23.5.25/Test'
 
 
